d760995096789c380b6b68e37add501661d113dd
yamazaki <yamazaki@KBSELAB19_26.kbselab.local>
2021-04-12 14:06:02 +0900

    ClassInfoクラスにListとかのフィールドを定義

--- src/element/ClassInfo.java
+++ src/element/ClassInfo.java
@@ -1,13 +1,20 @@
 package element;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ClassInfo {
 
-	String classname = new String();
-	List<String> fieldList = new ArrayList<String>();
-	List<String> methodList = new ArrayList<String>();
+	private String projectname = new String();
+	private String classname = new String();
+	private List<String> fieldList = new ArrayList<String>();
+	private List<String> methodList = new ArrayList<String>();
+	private Map<String, String> fieldtypemap = new HashMap<String,String>(); 					// Map<クラスの名前,フィールドの型>
+	private Map<String, String> fieldtypename = new HashMap<String,String>(); 					// Map<クラスの名前,フィールの名前>
+	private Map<String, List<String>> methodparatypemap = new HashMap<String,List<String>>(); 	// Map<メソッドの名前,メソッドの引数の型>
+	private Map<String, List<String>> methodparanamemap = new HashMap<String,List<String>>(); 	// Map<メソッドの名前,メソッドの引数の名前>
 
 	void setClassName(String classname)
 	{
